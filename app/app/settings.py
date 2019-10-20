5e4babd164000d92689394506cb44178bafd561f
OLVT01 <olvtwst01@gmail.com>
2019-10-20 18:13:22 +0900

    Added core app

--- app/app/settings.py
+++ app/app/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +143,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
